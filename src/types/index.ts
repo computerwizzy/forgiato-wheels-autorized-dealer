5e8cec2acbea20af68c59c209c996fda2b212b0d
computerwizzy <computerwizzypcrepair@gmail.com>
2026-04-27 23:33:41 -0500

    chore: add shared Wheel and QuoteFormData types

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Wheel {
+  name: string;
+  series: string;
+  imageUrl: string;
+  slug: string;
+}
+
+export interface QuoteFormData {
+  wheelName: string;
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
